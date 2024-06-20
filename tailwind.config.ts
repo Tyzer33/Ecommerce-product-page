246869ed35bdfc29c64391a47839d42a696f6b85
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-06-20 13:59:34 +0200

    chore: add Kumbh Sans font to the tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import type { Config } from 'tailwindcss'
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'kumbh-sans': ['"Kumbh Sans"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 } satisfies Config
