d5918357ee85cfb5dc4a169f7210fb103d3511a2
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-06-21 17:40:10 +0200

    refactor: move user avatar button in its own components

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,9 @@
-import { twJoin } from 'tailwind-merge'
 import logo from '../../assets/logo.svg'
-import avatar from '../../assets/image-avatar.png'
 import NavMenuButton from './NavMenuButton'
 import CartButton from './CartButton'
 import useBreakpoint from '../../utils/useBreakpoint'
 import NavLinks from './NavLinks'
+import UserAvatarButton from './UserAvatarButton'
 
 function Header() {
   const { isBelowLg, isAboveLg } = useBreakpoint('lg')
@@ -15,20 +14,7 @@ function Header() {
       <img src={logo} alt="Company logo" />
       {isAboveLg && <NavLinks type="header" />}
       <CartButton />
-      <button
-        type="button"
-        className={twJoin(
-          'ml-[1.375rem] rounded-full transition-[outline] lg:ml-[2.875rem]',
-          'outline outline-2 -outline-offset-2 outline-transparent',
-          'focus-visible:outline-accent hover:outline-accent',
-        )}
-      >
-        <img
-          className="aspect-square w-6 lg:w-[3.125rem]"
-          src={avatar}
-          alt="User avatar"
-        />
-      </button>
+      <UserAvatarButton />
     </header>
   )
 }
---
+++ src/components/Header/UserAvatarButton.tsx
@@ -0,0 +1,22 @@
+import { twJoin } from 'tailwind-merge'
+import avatar from '../../assets/image-avatar.png'
+
+function UserAvatarButton() {
+  return (
+    <button
+      type="button"
+      className={twJoin(
+        'ml-[1.375rem] rounded-full transition-[outline] lg:ml-[2.875rem]',
+        'outline outline-2 -outline-offset-2 outline-transparent',
+        'focus-visible:outline-accent hover:outline-accent',
+      )}
+    >
+      <img
+        className="aspect-square w-6 lg:w-[3.125rem]"
+        src={avatar}
+        alt="User avatar"
+      />
+    </button>
+  )
+}
+export default UserAvatarButton
