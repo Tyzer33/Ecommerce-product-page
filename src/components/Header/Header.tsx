d461a256510f7f05b286a6a35fd7656bc5b97d73
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-06-21 14:43:01 +0200

    feat: add desktop responsiveness to Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,15 +2,20 @@ import logo from '../../assets/logo.svg'
 import avatar from '../../assets/image-avatar.png'
 import NavMenuButton from './NavMenuButton'
 import CartButton from './CartButton'
+import useBreakpoint from '../../utils/useBreakpoint'
+import NavLinks from './NavLinks'
 
 function Header() {
+  const { isBelowLg, isAboveLg } = useBreakpoint('lg')
+
   return (
-    <header className="flex items-center px-6 pb-6 pt-5">
-      <NavMenuButton />
+    <header className="lg:border-main lg:border-b-1 flex items-center px-6 pb-6 pt-5">
+      {isBelowLg && <NavMenuButton />}
       <img src={logo} alt="Company logo" />
+      {isAboveLg && <NavLinks type="header" />}
       <CartButton />
       <img
-        className="ml-[1.375rem] aspect-square w-6 cursor-pointer rounded-full"
+        className="ml-[1.375rem] aspect-square w-6 cursor-pointer rounded-full lg:ml-[2.875rem] lg:w-[3.125rem]"
         src={avatar}
         alt="User avatar"
       />
