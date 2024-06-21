36d2311bb7f24fe9bf6b7ec746a4f9d1728b51fc
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-06-21 13:47:59 +0200

    feat: make number in cart badge bold

--- src/components/Header/CartButton.tsx
+++ src/components/Header/CartButton.tsx
@@ -13,7 +13,7 @@ function CartButton() {
     >
       <IconCart active={cartOpen || itemsInCart > 0} />
       {itemsInCart > 0 && (
-        <div className="absolute -right-[6px] -top-[6px] flex h-[.8125rem] w-[1.1875rem] items-center justify-center rounded-xl bg-accent text-[.625rem]/[.75rem] text-white">
+        <div className="absolute -right-[6px] -top-[6px] flex h-[.8125rem] w-[1.1875rem] items-center justify-center rounded-xl bg-accent text-[.625rem]/[.75rem] font-bold text-white">
           {itemsInCart}
         </div>
       )}
