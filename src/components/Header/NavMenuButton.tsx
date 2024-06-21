f3dec81198119705d905807dfe108f280eba04e8
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-06-21 12:10:40 +0200

    feat: add menu button logic to NavMenuButton

--- src/components/Header/NavMenuButton.tsx
+++ src/components/Header/NavMenuButton.tsx
@@ -1,14 +1,26 @@
+import { useState } from 'react'
+import { createPortal } from 'react-dom'
 import iconMenu from '../../assets/icon-menu.svg'
+
 function NavMenuButton() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
   return (
+    <>
       <button
         type="button"
         className="mr-4"
-        onClick={() => console.log('open menu')}
+        onClick={() => setIsMenuOpen(true)}
         aria-label="Open menu"
       >
         <img src={iconMenu} alt="Menu icon" />
       </button>
+      {isMenuOpen &&
+        createPortal(
+          <div>Menu</div>,
+          document.body,
+        )}
+    </>
   )
 }
 export default NavMenuButton
