81dc13bec0839e7b70afd6778ac70221e10bedd7
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-06-21 11:48:01 +0200

    feat: add cart badge
    add a cart badge with the number of items in cart

--- src/components/Header/CartButton.tsx
+++ src/components/Header/CartButton.tsx
@@ -12,6 +12,11 @@ function CartButton() {
       aria-label="Toggle cart"
     >
       <IconCart active={cartOpen || itemsInCart > 0} />
+      {itemsInCart > 0 && (
+        <div className="absolute -right-[6px] -top-[6px] flex h-[.8125rem] w-[1.1875rem] items-center justify-center rounded-xl bg-accent text-[.625rem]/[.75rem] text-white">
+          {itemsInCart}
+        </div>
+      )}
     </button>
   )
 }
