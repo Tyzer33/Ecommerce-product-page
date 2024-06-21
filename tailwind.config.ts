2139dd3b7f9b864199e819068b76313f17e0b0c8
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-06-21 11:39:09 +0200

    chore: add color variables in :root and tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,18 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      backgroundColor: {
+        main: 'hsla(var(--background))',
+        accent: 'hsla(var(--accent))',
+      },
+      textColor: {
+        body: 'hsla(var(--txt-body))',
+        heading: 'hsla(var(--txt-heading))',
+      },
+      fill: {
+        base: 'hsla(var(--fill-base))',
+        active: 'hsla(var(--fill-active))',
+      },
       fontFamily: {
         'kumbh-sans': ['"Kumbh Sans"', 'sans-serif'],
       },
