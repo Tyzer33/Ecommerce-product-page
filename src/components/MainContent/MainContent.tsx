2d830781835d187d41916d0d2aaa822592a08ca8
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-06-25 11:44:06 +0200

    feat: update paddings and margins media-queries  in MainContent

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -2,7 +2,7 @@ import ProductImages from './ProductImages'
 
 function MainContent() {
   return (
-    <main className="lg:mx-desktop px-12 py-[5.625rem]">
+    <main className="lg:mx-desktop lg:px-12 lg:py-[5.625rem]">
       <ProductImages />
     </main>
   )
