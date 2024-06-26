ad55284a3b2ed4d96f3af2d9ab45403eb17ccba2
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-06-26 18:01:43 +0200

    feat: add ProductImages component

--- src/components/MainContent/ProductImages.tsx
+++ src/components/MainContent/ProductImages.tsx
@@ -1,4 +1,34 @@
+import { useState } from 'react'
+import product from '../../data/products'
+import ProductImagesNavButton from './ProductImagesNavButton'
+import { cycleArray } from '../../utils/functions'
+
 function ProductImages() {
-  return <div>ProductImages</div>
+  const { images } = product
+  const [selectedImage, setSelectedImage] = useState(images[0].url)
+
+  function cycleImages(direction: 'prev' | 'next') {
+    const index = images.findIndex(({ url }) => url === selectedImage)
+    const newImage = cycleArray(images, index, direction)
+    setSelectedImage(newImage.url)
+  }
+
+  return (
+    <div className="relative bg-black">
+      <img
+        className="m-auto aspect-[375/300] max-h-[31.25rem] object-cover"
+        src={selectedImage}
+        alt="Selected"
+      />
+      <ProductImagesNavButton
+        type="prev"
+        handleClick={() => cycleImages('prev')}
+      />
+      <ProductImagesNavButton
+        type="next"
+        handleClick={() => cycleImages('next')}
+      />
+    </div>
+  )
 }
 export default ProductImages
