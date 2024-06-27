84ee10c863d8ea19e1bb58e59dc51e968b47bcc2
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-06-27 18:19:34 +0200

    refactor: improve NavMenu behavior
    add overflow hidden and inert to the rest of the site when menu is open and replace position absolute by fixed to prevent unwanted positionnement

--- src/components/Header/NavMenu.tsx
+++ src/components/Header/NavMenu.tsx
@@ -1,9 +1,21 @@
+import { useEffect } from 'react'
 import iconClose from '../../assets/icon-close.svg'
 import NavLinks from './NavLinks'
 
 function NavMenu({ closeMenu }: Props) {
+  useEffect(() => {
+    const root = document.querySelector('#root') as HTMLElement
+
+    root.setAttribute('inert', 'true')
+    document.body.style.overflow = 'hidden'
+    return () => {
+      root.removeAttribute('inert')
+      document.body.style.overflow = 'auto'
+    }
+  }, [])
+
   return (
-    <div className="absolute inset-0 bg-black/50">
+    <div className="fixed inset-0 bg-black/50">
       <section className="absolute inset-0 right-auto w-60 space-y-12 bg-main p-6">
         <button type="button" onClick={closeMenu}>
           <img src={iconClose} alt="Close menu" />
