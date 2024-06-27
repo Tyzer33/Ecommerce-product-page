8f57ab95137c0424a21bce6a7bf18dd3bb9bca5e
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-06-27 16:41:13 +0200

    feat: add html structure to TextContent

--- src/components/MainContent/TextContent/TextContent.tsx
+++ src/components/MainContent/TextContent/TextContent.tsx
@@ -1,4 +1,29 @@
+import { useState } from 'react'
+import product from '../../../data/products'
+
 function TextContent() {
-  return <div>TextContent</div>
+  const [count, setCount] = useState(0)
+  const { brand, name, desc, price, discount } = product
+
+  return (
+    <section>
+      <h2>{brand}</h2>
+      <h1>{name}</h1>
+      <p>{desc}</p>
+      <div>
+        <p>
+          {price * discount}
+          <span>{discount * 100}%</span>
+        </p>
+        <span>{price}</span>
+      </div>
+      <div>
+        <button type="button">-</button>
+        <span>{count}</span>
+        <button type="button">+</button>
+      </div>
+      <button type="button">add to cart</button>
+    </section>
+  )
 }
 export default TextContent
