274b3cee93ca526e6e2ffdf9e017b19982359e75
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-06-27 15:24:19 +0200

    feat: add responsive image navigation to ProductImages component

--- src/components/MainContent/ProductImages.tsx
+++ src/components/MainContent/ProductImages.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
+import { twJoin } from 'tailwind-merge'
 import product from '../../data/products'
 import ProductImagesNavButton from './ProductImagesNavButton'
 import { cycleArray } from '../../utils/functions'
+import useBreakpoint from '../../utils/useBreakpoint'
 
 function ProductImages() {
   const { images } = product
   const [selectedImage, setSelectedImage] = useState(images[0])
+  const { isAboveLg, isBelowLg } = useBreakpoint('lg')
 
   function cycleImages(direction: 'prev' | 'next') {
     const index = images.findIndex(({ id }) => id === selectedImage.id)
@@ -14,20 +17,48 @@ function ProductImages() {
   }
 
   return (
-    <div className="relative bg-black">
+    <div className="relative max-lg:bg-black lg:max-w-[27.8125rem] lg:space-y-8">
       <img
-        className="m-auto aspect-[375/300] max-h-[31.25rem] object-cover"
+        className="m-auto max-h-[31.25rem] object-cover max-lg:aspect-[375/300] lg:rounded-2xl"
         src={selectedImage.url}
         alt="Selected"
       />
-      <ProductImagesNavButton
-        type="prev"
-        handleClick={() => cycleImages('prev')}
-      />
-      <ProductImagesNavButton
-        type="next"
-        handleClick={() => cycleImages('next')}
-      />
+      {isBelowLg && (
+        <>
+          <ProductImagesNavButton
+            type="prev"
+            handleClick={() => cycleImages('prev')}
+          />
+          <ProductImagesNavButton
+            type="next"
+            handleClick={() => cycleImages('next')}
+          />
+        </>
+      )}
+      {isAboveLg && (
+        <div className="flex justify-between">
+          {images.map((image) => (
+            <button
+              key={image.id}
+              className={twJoin(
+                'relative aspect-square w-[5.5rem] overflow-hidden rounded-[.625rem] transition-[box-shadow] focus-visible:outline-none',
+                'after:absolute after:inset-0 after:transition-colors',
+                selectedImage.id === image.id
+                  ? 'ring-accent after:bg-thumbnail-active ring-2'
+                  : 'hover:after:bg-thumbnail-hover focus-visible:after:bg-thumbnail-hover',
+              )}
+              type="button"
+              onClick={() => setSelectedImage(image)}
+              aria-label={`Select image ${image.id}`}
+            >
+              <img
+                src={image.thumbnailUrl}
+                alt={`Product Thumbnail ${image.id}`}
+              />
+            </button>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
