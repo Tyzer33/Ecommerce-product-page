04fd1983375d6f37f5ede1bbb4ca8f7004956310
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-06-28 13:37:14 +0200

    refactor: move margins and max width to div#root

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,7 +9,7 @@ function Header() {
   const { isBelowLg, isAboveLg } = useBreakpoint('lg')
 
   return (
-    <header className="lg:mx-desktop flex items-center px-6 pb-6 pt-5 lg:border-b-1 lg:border-main lg:pb-[2.125rem] lg:pt-7">
+    <header className="flex items-center px-6 pb-6 pt-5 lg:border-b-1 lg:border-main lg:pb-[2.125rem] lg:pt-7">
       {isBelowLg && <NavMenuButton />}
       <img src={logo} alt="Company logo" />
       {isAboveLg && <NavLinks type="header" />}
--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -3,7 +3,7 @@ import TextContent from './TextContent/TextContent'
 
 function MainContent() {
   return (
-    <main className="lg:mx-desktop lg:px-12 lg:py-[5.625rem]">
+    <main className="lg:px-12 lg:py-[5.625rem]">
       <ProductImages />
       <TextContent />
     </main>
