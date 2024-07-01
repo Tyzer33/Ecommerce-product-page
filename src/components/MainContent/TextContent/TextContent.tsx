132b8411473421f516334bcb995ec22b9b5c225d
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-07-01 12:05:31 +0200

    feat: update TextContent layout and styling for large screens

--- src/components/MainContent/TextContent/TextContent.tsx
+++ src/components/MainContent/TextContent/TextContent.tsx
@@ -14,25 +14,25 @@ function TextContent() {
   const { brand, name, desc, price, discount } = product
 
   return (
-    <section className="m-6">
+    <section className="max-lg:m-6 lg:mt-[clamp(0rem,-18.5421rem+28.972vi,3.875rem)] lg:max-w-[27.75rem] lg:flex-1">
       <h2>{brand}</h2>
-      <h1 className="mb-4 mt-3">{name}</h1>
+      <h1 className="mb-4 mt-3 lg:mb-9 lg:mt-5">{name}</h1>
       <p>{desc}</p>
-      <div className="my-6 flex items-center justify-between">
-        <div className="flex items-center gap-4">
-          <span className="text-[1.75rem] font-bold tracking-[0.035em] text-heading">
+      <div className="my-6 max-lg:flex max-lg:items-center max-lg:justify-between lg:mb-8 lg:space-y-3">
+        <div className="flex items-end gap-4">
+          <span className="text-[1.75rem]/[2rem] font-bold tracking-[0.035em] text-heading">
             {formatter.format(price * discount)}
           </span>
-          <span className="text-badge bg-badge rounded-md px-[.625rem] py-1 text-base font-bold">
+          <span className="rounded-md bg-badge px-[.625rem] py-1 text-base/5 font-bold text-badge">
             {discount * 100}%
           </span>
         </div>
-        <span className="font-bold tracking-[0.02em] line-through">
+        <span className="block font-bold tracking-[0.02em] line-through">
           {formatter.format(price)}
         </span>
       </div>
-      <div className="space-y-4">
-        <div className="bg-alt flex h-14 w-full items-center justify-between rounded-[.625rem] px-6 text-base font-bold text-heading">
+      <div className="flex gap-4 max-lg:flex-col">
+        <div className="flex h-14 w-full items-center justify-between rounded-[.625rem] bg-alt px-6 text-base font-bold text-heading lg:flex-[2]">
           <button
             type="button"
             onClick={() => setCount((prev) => (prev > 0 ? prev - 1 : 0))}
@@ -51,9 +51,9 @@ function TextContent() {
         </div>
         <button
           type="button"
-          className="flex h-14 w-full items-center justify-center gap-4 rounded-[.625rem] bg-accent fill-base font-bold text-heading"
+          className="flex h-14 w-full items-center justify-center gap-4 rounded-[.625rem] bg-accent fill-base font-bold text-heading lg:flex-[5]"
         >
-          <IconCart />
+          <IconCart size="small" />
           Add to cart
         </button>
       </div>
