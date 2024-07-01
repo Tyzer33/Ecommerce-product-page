96c88f89b4716cf6734c8100945255f4399d99ad
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-07-01 23:36:29 +0200

    feat: add lightbox gallery functionality to ProductImagesDesktop component

--- src/components/MainContent/ProductImages/ProductImagesDesktop.tsx
+++ src/components/MainContent/ProductImages/ProductImagesDesktop.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+import { createPortal } from 'react-dom'
 import { ProductImagesProps } from '../../../@types/types'
 import ProductThumbnails from './ProductThumbnails'
 
@@ -6,17 +8,27 @@ function ProductImagesDesktop({
   selectedImage,
   setSelectedImage,
 }: ProductImagesProps) {
+  const [isLightBoxOpen, setIsLightBoxOpen] = useState(false)
+
   return (
-    <div className="max-w-[27.8125rem] flex-1 space-y-8">
-      <button type="button" className="block overflow-hidden rounded-2xl">
-        <img src={selectedImage.url} alt="Selected" />
-      </button>
-      <ProductThumbnails
-        images={images}
-        selectedImage={selectedImage}
-        setSelectedImage={setSelectedImage}
-      />
-    </div>
+    <>
+      <div className="max-w-[27.8125rem] flex-1 space-y-8">
+        <button
+          type="button"
+          className="block overflow-hidden rounded-2xl"
+          onClick={() => setIsLightBoxOpen(true)}
+        >
+          <img src={selectedImage.url} alt="Selected" />
+        </button>
+        <ProductThumbnails
+          images={images}
+          selectedImage={selectedImage}
+          setSelectedImage={setSelectedImage}
+        />
+      </div>
+      {isLightBoxOpen &&
+        createPortal(<div>Lightbox Gallery</div>, document.body)}
+    </>
   )
 }
 export default ProductImagesDesktop
