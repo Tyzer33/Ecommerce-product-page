cbaf6062b33fbf0e28b6f09713bd67ff06ca8b63
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-07-03 11:02:40 +0200

    feat: Add useCartContext hook for managing cart state

--- src/utils/useCustomContext.ts
+++ src/utils/useCustomContext.ts
@@ -1,7 +1,8 @@
 /* eslint-disable import/prefer-default-export */
 import { useContext } from 'react'
 import ProductImagesContext from '../context/ProductImagesContext/ProductImagesContext'
-import { ProductImagesContextType } from '../@types/types'
+import { CartContextType, ProductImagesContextType } from '../@types/types'
+import CartContext from '../context/CartContext/CartContext'
 
 export function useProductImagesContext() {
   const context = useContext(ProductImagesContext) as ProductImagesContextType
@@ -9,6 +10,14 @@ export function useProductImagesContext() {
     throw new Error(
       'this useProductImagesContext must be used within his provider',
     )
+  }
+  return context
+}
+
+export function useCartContext() {
+  const context = useContext(CartContext) as CartContextType
+  if (!context) {
+    throw new Error('this useCartContext must be used within his provider')
   }
   return context
 }
