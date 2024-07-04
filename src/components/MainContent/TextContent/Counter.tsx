44f99a7553969d46b6bfcf23e46872da258aa241
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-07-04 16:59:51 +0200

    feat: make click on Counter buttons easier

--- src/components/MainContent/TextContent/Counter.tsx
+++ src/components/MainContent/TextContent/Counter.tsx
@@ -3,10 +3,10 @@ import IconPlus from './IconPlus'
 
 function Counter({ quantity, add, reduce }: Props) {
   return (
-    <div className="flex h-14 w-full items-center justify-between rounded-[.625rem] bg-alt px-6 text-base font-bold text-heading lg:flex-[2]">
+    <div className="flex h-14 w-full min-w-28 items-center justify-between rounded-[.625rem] bg-alt text-base font-bold text-heading lg:flex-[2]">
       <button
         type="button"
-        className="aspect-square fill-accent transition-colors hover:fill-accent-hover"
+        className="flex h-full items-center justify-center fill-accent px-6 transition-colors hover:fill-accent-hover lg:px-4"
         onClick={reduce}
         aria-label="minus"
       >
@@ -15,7 +15,7 @@ function Counter({ quantity, add, reduce }: Props) {
       <span>{quantity}</span>
       <button
         type="button"
-        className="aspect-square fill-accent transition-colors hover:fill-accent-hover"
+        className="flex h-full items-center justify-center fill-accent px-6 transition-colors hover:fill-accent-hover lg:px-4"
         onClick={add}
         aria-label="plus"
       >
