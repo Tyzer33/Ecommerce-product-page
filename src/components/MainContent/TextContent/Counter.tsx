7adc8c4e8265e41954b7b33c9f9883c50ada9940
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-07-05 15:39:02 +0200

    feat: add focus styles to Counter

--- src/components/MainContent/TextContent/Counter.tsx
+++ src/components/MainContent/TextContent/Counter.tsx
@@ -1,12 +1,16 @@
+import { twJoin } from 'tailwind-merge'
 import IconMinus from './IconMinus'
 import IconPlus from './IconPlus'
 
 function Counter({ quantity, add, reduce }: Props) {
   return (
-    <div className="flex h-14 w-full min-w-28 items-center justify-between rounded-[.625rem] bg-alt text-base font-bold text-heading lg:flex-[2]">
+    <div className="flex h-14 w-full min-w-28 items-center justify-between overflow-hidden rounded-[.625rem] bg-alt text-base font-bold text-heading lg:flex-[2]">
       <button
         type="button"
-        className="flex h-full items-center justify-center fill-accent px-6 transition-colors hover:fill-accent-hover lg:px-4"
+        className={twJoin(
+          'flex h-full items-center justify-center rounded-none fill-accent px-6 transition-colors lg:px-4',
+          'hover:fill-accent-hover focus-visible:bg-black/5 focus-visible:fill-accent-hover focus-visible:outline-none',
+        )}
         onClick={reduce}
         aria-label="minus"
       >
@@ -15,7 +19,10 @@ function Counter({ quantity, add, reduce }: Props) {
       <span>{quantity}</span>
       <button
         type="button"
-        className="flex h-full items-center justify-center fill-accent px-6 transition-colors hover:fill-accent-hover lg:px-4"
+        className={twJoin(
+          'flex h-full items-center justify-center rounded-none fill-accent px-6 transition-colors lg:px-4',
+          'hover:fill-accent-hover focus-visible:bg-black/5 focus-visible:fill-accent-hover focus-visible:outline-none',
+        )}
         onClick={add}
         aria-label="plus"
       >
