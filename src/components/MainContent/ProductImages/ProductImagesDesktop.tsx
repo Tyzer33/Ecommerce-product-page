474bed5e6ec421f5f08fa18d14809b0c4cdcd492
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-07-05 15:38:02 +0200

    feat: add focus styles to ProductImagesDesktop

--- src/components/MainContent/ProductImages/ProductImagesDesktop.tsx
+++ src/components/MainContent/ProductImages/ProductImagesDesktop.tsx
@@ -13,7 +13,7 @@ function ProductImagesDesktop() {
       <div className="max-w-[27.8125rem] flex-1 space-y-8">
         <button
           type="button"
-          className="block overflow-hidden rounded-2xl"
+          className="block overflow-hidden rounded-2xl focus-visible:outline-4 focus-visible:outline-accent"
           onClick={() => setIsLightBoxOpen(true)}
         >
           <img src={selectedImage.url} alt="Selected" />
