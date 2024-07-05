15183c8479150ff7f0b63c4e3038452f5fb7d10c
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-07-05 15:38:47 +0200

    feat: add focus styles to AddButton

--- src/components/MainContent/TextContent/AddButton.tsx
+++ src/components/MainContent/TextContent/AddButton.tsx
@@ -1,3 +1,4 @@
+import { twJoin } from 'tailwind-merge'
 import { useCartContext } from '@/utils/useCustomContext'
 import IconCart from '@/components/common/IconCart'
 
@@ -7,7 +8,10 @@ function AddButton({ quantity }: Props) {
   return (
     <button
       type="button"
-      className="flex h-14 w-full items-center justify-center gap-4 rounded-[.625rem] bg-accent fill-current font-bold text-heading transition-colors hover:bg-accent-hover lg:flex-[5]"
+      className={twJoin(
+        'flex h-14 w-full items-center justify-center gap-4 rounded-[.625rem] bg-accent fill-current font-bold text-heading transition-colors lg:flex-[5]',
+        'focus-within:bg-accent-hover focus-within:outline-none hover:bg-accent-hover',
+      )}
       onClick={() => quantity > 0 && addToCart('553560-093', quantity)}
     >
       <IconCart size="small" />
