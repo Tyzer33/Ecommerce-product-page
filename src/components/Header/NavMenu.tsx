f2a4622b17aebd9b564c3d97ba5cbc41b00cae0a
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-07-08 09:41:30 +0200

    feat: add animation when opening and closing menu

--- src/components/Header/NavMenu.tsx
+++ src/components/Header/NavMenu.tsx
@@ -1,8 +1,12 @@
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import NavLinks from './NavLinks'
 import IconClose from '../MainContent/ProductImages/IconClose'
+import { getRootVariable, isMsString, msStringToNum } from '@/utils/functions'
 
 function NavMenu({ closeMenu }: Props) {
+  const wrapperRef = useRef<HTMLDivElement>(null)
+  const menuRef = useRef<HTMLDivElement>(null)
+
   useEffect(() => {
     const root = document.querySelector('#root') as HTMLElement
 
@@ -14,13 +18,25 @@ function NavMenu({ closeMenu }: Props) {
     }
   }, [])
 
+  function animateClose() {
+    const durationStr = getRootVariable('--menu-animation-duration')
+    const durationNum = isMsString(durationStr) ? msStringToNum(durationStr) : 0
+
+    wrapperRef.current?.classList.add('animate-fade-out')
+    menuRef.current?.classList.add('animate-slide-out-l')
+    setTimeout(closeMenu, durationNum)
+  }
+
   return (
-    <div className="fixed inset-0 bg-black/50">
-      <section className="absolute inset-0 right-auto w-60 space-y-12 bg-main p-6">
+    <div ref={wrapperRef} className="animate-fade-in fixed inset-0 bg-black/50">
+      <section
+        ref={menuRef}
+        className="animate-slide-in-l absolute inset-0 right-auto w-60 space-y-12 bg-main p-6"
+      >
         <button
           className="block fill-current"
           type="button"
-          onClick={closeMenu}
+          onClick={animateClose}
           aria-label="Close menu"
         >
           <IconClose size="small" />
